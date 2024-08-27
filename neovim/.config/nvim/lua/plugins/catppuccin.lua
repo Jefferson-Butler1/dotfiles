bd731542e21a0cd06f53956ae8b2f1a042985066
Jefferson-Butler1 <jefferson.butler@icloud.com>
2024-08-27 11:30:32 -0700

    fix i3 and add basic nvim stuff
    nvim:
      treeshitter
      telescope
      lazy
      catppuccin
      neotree

--- neovim/.config/nvim/lua/plugins/catppuccin.lua
+++ neovim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,10 +1,11 @@
-return 
+return {
   {
     "catppuccin/nvim",
-    lazy=false,
+    lazy = false,
     name = "catppuccin",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme "catppuccin"
+      vim.cmd.colorscheme "catppuccin-mocha"
     end
   }
+}
--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,14 @@
-return 
+return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        ensure_installed = { "lua", "javascript", "typescript" },
+        auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
       })
     end
-  } 
+  }
+}
