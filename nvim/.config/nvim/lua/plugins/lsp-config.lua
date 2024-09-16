99251549127a6a275476a3b0660a3ca46477fc43
Jefferson-Butler1 <jbutler@questforward.org>
2024-09-15 17:45:27 -0700

    add completions

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,12 +1,12 @@
 return {
   {
-  "williamboman/mason.nvim",
-  config = function()
-    require("mason").setup()
-  end
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
   },
   {
-   "williamboman/mason-lspconfig.nvim",
+    "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = {
@@ -14,40 +14,143 @@ return {
           "clangd",
           "tailwindcss",
           "graphql",
-          "ts_ls", -- or eslint
+          "ts_ls",  -- maybe use eslint_d?
           "pyright",
           "rust_analyzer",
         }
-     })
+      })
     end
   },
   {
     "neovim/nvim-lspconfig",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+    },
     config = function()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.clangd.setup({})
-      lspconfig.tailwindcss.setup({})
-      lspconfig.graphql.setup({})
-      lspconfig.ts_ls.setup({})
-      lspconfig.pyright.setup({})
-      lspconfig.rust_analyzer.setup({})
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+      -- Setup for each LSP
+      local servers = { "lua_ls", "clangd", "tailwindcss", "graphql", "ts_ls", "pyright", "rust_analyzer" }
+      for _, lsp in ipairs(servers) do
+        lspconfig[lsp].setup({
+          capabilities = capabilities,
+        })
+      end
 
-
-      vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-      vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
-      vim.keymap.set('n', 'gi', function() vim.lsp.buf.implementation() end, opts)
-      vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-      vim.keymap.set('n', '<leader>ca', function() vim.lsp.buf.code_action() end, opts)
-
-      vim.keymap.set('i', '<C-k>', function() vim.lsp.buf.signature_help() end, opts)
-      vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
-      vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
-      vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
-      vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
-      vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
-      vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
+      -- Keymaps
+      local opts = { noremap = true, silent = true }
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+      vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help, opts)
+      vim.keymap.set('n', '<leader>vws', vim.lsp.buf.workspace_symbol, opts)
+      vim.keymap.set('n', '<leader>vd', vim.diagnostic.open_float, opts)
+      vim.keymap.set('n', '[d', vim.diagnostic.goto_next, opts)
+      vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, opts)
+      vim.keymap.set('n', '<leader>vrr', vim.lsp.buf.references, opts)
+      vim.keymap.set('n', '<leader>vrn', vim.lsp.buf.rename, opts)
+      
+      -- Formatting keymap
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, opts)
     end
-  }
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "onsails/lspkind.nvim",
+      {
+        "windwp/nvim-autopairs",
+        config = function()
+          require("nvim-autopairs").setup({})
+        end,
+      },
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      local lspkind = require("lspkind")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered({
+            border = "rounded",
+            winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
+          }),
+          documentation = cmp.config.window.bordered({
+            border = "rounded",
+            winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
+          }),
+        },
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = "symbol",
+            maxwidth = 50,
+            ellipsis_char = "...",
+          }),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<C-j>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<C-k>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+      -- Set up auto-pairs
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
+  },
 }
