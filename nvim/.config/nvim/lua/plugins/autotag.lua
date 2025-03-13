9a79139beb98b065f054874cd9dc758932a3cd39
Jefferson-Butler1 <jefferson.butler@icloud.com>
2025-03-13 13:35:26 -0700

    formatting and such

--- nvim/.config/nvim/lua/plugins/autotag.lua
+++ nvim/.config/nvim/lua/plugins/autotag.lua
@@ -1,14 +1,22 @@
 return {
-    "windwp/nvim-ts-autotag",
-    event = "InsertEnter",
-    dependencies = "nvim-treesitter/nvim-treesitter",
-    config = function()
-        require('nvim-ts-autotag').setup({
-            enable = true,
-            filetypes = { 
-                'html', 'javascript', 'typescript', 'javascriptreact', 
-                'typescriptreact', 'tsx', 'jsx', 'xml', 'php', 'markdown'
-            },
-        })
-    end,
+	"windwp/nvim-ts-autotag",
+	event = "InsertEnter",
+	dependencies = "nvim-treesitter/nvim-treesitter",
+	config = function()
+		require("nvim-ts-autotag").setup({
+			enable = true,
+			filetypes = {
+				"html",
+				"javascript",
+				"typescript",
+				"javascriptreact",
+				"typescriptreact",
+				"tsx",
+				"jsx",
+				"xml",
+				"php",
+				"markdown",
+			},
+		})
+	end,
 }
