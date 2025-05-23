8f5f1c1a1aa02dc6d951761a54d0e13df8035775
Jefferson-Butler1 <jefferson.butler@icloud.com>
2025-05-22 21:53:52 -0700

    Add modern productivity and navigation plugins
    - Add Smear-cursor for smooth cursor animations
    - Upgrade to Harpoon2 with improved API and lazy loading
    - Add Leap for enhanced navigation with jump labels
    - Add Mini.animate for smooth scrolling and window animations
    - Add Mini.files as modern file explorer with preview
    - Add Mini.surround for quick bracket/quote manipulation
    - Add Todo-comments for highlighting and navigating TODO/FIXME
    - Add Trouble for better diagnostics and quickfix UI

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -1,35 +1,42 @@
 return {
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
+  opts = {
+    menu = {
+      width = vim.api.nvim_win_get_width(0) - 4,
+    },
+    settings = {
+      save_on_toggle = true,
+    },
   },
-  config = function()
-    local harpoon = require("harpoon")
-    harpoon:setup({
-      settings = {
-        save_on_toggle = false,
-        sync_on_ui_close = false,
+  keys = function()
+    local keys = {
+      {
+        "<leader>H",
+        function()
+          require("harpoon"):list():add()
+        end,
+        desc = "Harpoon File",
       },
-    })
+      {
+        "<leader>h",
+        function()
+          local harpoon = require("harpoon")
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "Harpoon Quick Menu",
+      },
+    }
 
-
-    vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = "Harpoon add file" })
-    vim.keymap.set("n", "<leader>hm", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon toggle menu" })
-
-    -- Navigate to specific files
-    vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end, { desc = "Harpoon file 1" })
-    vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end, { desc = "Harpoon file 2" })
-    vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end, { desc = "Harpoon file 3" })
-    vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end, { desc = "Harpoon file 4" })
-
-    -- Sequential navigation
-    vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end, { desc = "Harpoon previous" })
-    vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end, { desc = "Harpoon next" })
-
-    -- File management
-    vim.keymap.set("n", "<leader>hr", function() harpoon:list():remove() end, { desc = "Harpoon remove file" })
-    vim.keymap.set("n", "<leader>hc", function() harpoon:list():clear() end, { desc = "Harpoon clear all" })
-
+    for i = 1, 5 do
+      table.insert(keys, {
+        "<leader>" .. i,
+        function()
+          require("harpoon"):list():select(i)
+        end,
+        desc = "Harpoon to File " .. i,
+      })
+    end
+    return keys
   end,
 }
