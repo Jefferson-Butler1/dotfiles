00a0c32dc1c08c5a6de4b5ff102b6ca288a04646
Jefferson-Butler1 <jefferson.butler@icloud.com>
2025-08-06 16:45:58 -0700

    wakatime for fun

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -57,6 +57,7 @@ vim.pack.add({
   { src = "https://github.com/akinsho/toggleterm.nvim" },
   { src = "https://github.com/sphamba/smear-cursor.nvim" },
   { src = "https://github.com/folke/which-key.nvim" },
+  { src = "https://github.com/wakatime/vim-wakatime" },
 })
 
 -- ============================================================================
